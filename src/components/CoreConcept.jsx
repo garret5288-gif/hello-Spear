3fb4555cc62172f2881f8c4e35d0b8be36b83972
Garret Adkins <garret5288@gmail.com>
2026-01-22 14:04:51 -0500

    added new react project and finished old one

--- src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcept({ title, description, img }) {
-  return (
-    <li>
-      <img src={img} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
