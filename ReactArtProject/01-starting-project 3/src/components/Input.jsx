5e3f769e49bf63497ed53b503b6dc642b996e2d6
Garret Adkins <garret5288@gmail.com>
2026-01-28 13:06:59 -0500

    added fonts and colors to input and label classes

--- ReactArtProject/01-starting-project 3/src/components/Input.jsx
+++ ReactArtProject/01-starting-project 3/src/components/Input.jsx
@@ -1,8 +1,20 @@
 export default function Input({ label, invalid, ...props }) {
+    let labelClasses = 
+      "block mb-2 text-xs font-bold tracking-wide uppercase";
+    let inputClasses = '"w-full px-3 py-2 leading-tight bg-stone-300 text-gray-700 border rounded shadow"'
+    
+    if (invalid) {
+      labelClasses += ' text-red-400';
+      inputClasses += ' text-red-500 bg-red-100 border-red-300';
+    } else {
+        labelClasses += ' text-stone-300'
+        inputClasses += ' text-gray-700 bg-stone-300';
+    }
+
     return (
         <>
-            <label className="block mb-2 text-xs font-bold tracking-wide text-stone-200 uppercase" $invalid={invalid}>{label}</label>
-            <input className="w-full px-3 py-2 leading-tight bg-stone-300 text-gray-700 border rounded shadow" {...props} />
+         <label className={labelClasses}>{label}</label>
+         <input className={inputClasses} {...props} />
         </>
     );
 }
