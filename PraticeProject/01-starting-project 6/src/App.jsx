c7a88d7a7ae65a80c207b40582b73e99ff385340
Garret Adkins <garret5288@gmail.com>
2026-01-30 09:54:46 -0500

    worked more on my practice projects
    still working on my project app adding states and ref's

--- PraticeProject/01-starting-project 6/src/App.jsx
+++ PraticeProject/01-starting-project 6/src/App.jsx
@@ -1,11 +1,51 @@
+import { useState } from "react";
+
 import NewProject from "./Components/NewProject";
-import ProjectSidebar from "./Components/ProjectsSidebar";
+import ProjectSidebar from "./Components/ProjectsSidebar.jsx";
+import NoProjectSelected from "./Components/NoProjectSelected.jsx";
 
 function App() {
+  const [projectsState, setProjectState] = useState({
+    selectedProjectId: undedefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject],
+      }
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+
   return (
     <main className='h-screen my-8 flex'>
-      <ProjectSidebar />
-      <NewProject />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
     );
 }
--- PraticeProject/01-starting-project 6/src/Components/NewProject.jsx
+++ PraticeProject/01-starting-project 6/src/Components/NewProject.jsx
@@ -1,17 +1,59 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
-    return (
-        <div className='w-[35rem] mt-16 '>
-            <menu className='flex item-center justify-end gap-4 my-4'>
-                <li><button className='text-stone-800 hover:text-stone-950'>Cancel</button></li>
-                <li><button className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'>Save</button></li>
-            </menu>
-            <div>
-                <Input label='Title' />
-                <Input label='Description' textarea />
-                <Input label='Due Date' />
-            </div>
+export default function NewProject({onAdd}) {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const dueDateRef = useRef();
+
+  function handleSave() {
+    const enteredTitle = titleRef.current.value;
+    const enteredDescription = descriptionRef.current.value;
+    const enteredDueDate = dueDateRef.current.value;
+
+    //validation ...
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate
+    });
+  }
+
+  return (
+    <div className='w-[35rem] mt-16'>
+      <menu className='flex item-center justify-end gap-4 my-4'>
+        <li>
+          <button className='text-stone-800 hover:text-stone-950'>
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button 
+          className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'
+          onClick={handleSave}> 
+            Save
+          </button>
+        </li>
+      </menu>
+      <div className='w-[35rem] mt-16'>
+        <menu className='flex item-center justify-end gap-4 my-4'>
+          <li>
+            <button className='text-stone-800 hover:text-stone-950'>
+                Cancel
+            </button>
+            </li>
+            <li>
+              <button className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'>
+                Save
+              </button>
+            </li>
+          </menu>
+          <div>
+            <Input type='text' ref={title} label='Title' />
+            <Input ref={description} label='Description' textarea />
+            <Input type='date' ref={dueDate} label='Due Date' />
+          </div>
         </div>
-    )
+    );
 }
